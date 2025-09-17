adb241c84b35a78f0dd9fbde6e14482ebd0d728b
Haven Madray <sgpublic2002@gmail.com>
2025-09-17 15:51:21 +0800

    feat(flavor): add flavor for Cudy TR3000 256m

---
+++ flavor/Cudy_TR3000/README.md
@@ -0,0 +1,17 @@
+# Cudy TR3000
+
+## 适用于
+
++ [Cudy TR3000 256m](https://www.cudy.com/zh-cn/products/tr3000-1-0)
+
+## 主题
+
+[jerrykuku/luci-theme-argon: Argon is a clean HTML5 theme for OpenWrt LuCI. Users may setup their own favorite logins, including beautiful pics and customized mp4 videos. (github.com)](https://github.com/jerrykuku/luci-theme-argon)
+
+![screenshot_pc.jpg (1767×938) (raw.githubusercontent.com)](https://raw.githubusercontent.com/jerrykuku/luci-theme-argon/master/Screenshots/screenshot_pc.jpg)
+
+## 管理页
+
+默认地址：`192.168.7.1`
+
+默认密码：`password`
---
+++ flavor/Cudy_TR3000/diy-part2.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part2.sh
+# Description: OpenWrt DIY script part 2 (After Update feeds)
+#
+
+set -e
+
+. $COMMON_DIY_P2
+
+##    添加你的自定义逻辑    ##
+
+# 修改默认主题
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
+sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-nginx/Makefile
+
+# 删除内置 frp
+rm -rf feeds/packages/net/frp
+# 使用最新 GoLang
+rm -rf feeds/packages/lang/golang
+cp -a ./custom-feeds/packages-sbwml/lang/golang feeds/packages/lang/golang
+# 使用最新 v2ray-geodata
+rm -rf feeds/packages/net/v2ray-geodata
+cp -a ./custom-feeds/packages-sbwml/net/v2ray-geodata feeds/packages/net/v2ray-geodata
+# 使用 luci-app-zerotier
+rm -rf feeds/luci/applications/luci-app-zerotier
+cp -a ./custom-feeds/luci-immortalwrt/applications/luci-app-zerotier feeds/luci/applications/luci-app-zerotier
