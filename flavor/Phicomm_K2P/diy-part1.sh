3906df634270e5e7dd32f5b202926a6347814a38
Haven Madray <sgpublic2002@gmail.com>
2025-09-20 08:17:08 +0800

    feat(Phicomm_K2P): add zerotier

--- flavor/Phicomm_K2P/diy-part1.sh
+++ flavor/Phicomm_K2P/diy-part1.sh
@@ -26,13 +26,11 @@ fi
 
 # 拉取最新 GoLang
 git_clone https://github.com/sbwml/packages_lang_golang ./custom-feeds/packages-sbwml/lang/golang 24.x
-# 拉取源 lede
-git_clone https://github.com/coolsnowwolf/lede ./custom-feeds/lede
+# 拉取源 immortalwrt-luci
+git_clone https://github.com/immortalwrt/luci ./custom-feeds/luci-immortalwrt $IMMORTALWRT_BRANCH
 
 
 # 拉取主题 luci-theme-argon
 git_clone https://github.com/jerrykuku/luci-theme-argon.git package/jerrykuku/luci-theme-argon
 # 拉取插件 luci-app-argon-config
 git_clone https://github.com/jerrykuku/luci-app-argon-config.git package/jerrykuku/luci-app-argon-config
-# 拉取插件 luci-app-eqosplus
-git_clone https://github.com/sirpdboy/luci-app-eqosplus package/sirpdboy/luci-app-eqosplus
--- flavor/Phicomm_K2P/diy-part2.sh
+++ flavor/Phicomm_K2P/diy-part2.sh
@@ -20,8 +20,10 @@ set -e
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci/Makefile
 sed -i 's/luci-theme-bootstrap/luci-theme-argon/g' feeds/luci/collections/luci-nginx/Makefile
 
-# 删除内置 frp
-rm -rf feeds/packages/net/frp
 # 使用最新 GoLang
 rm -rf feeds/packages/lang/golang
 cp -a ./custom-feeds/packages-sbwml/lang/golang feeds/packages/lang/golang
+
+# 使用 luci-app-zerotier
+rm -rf feeds/luci/applications/luci-app-zerotier
+cp -a ./custom-feeds/luci-immortalwrt/applications/luci-app-zerotier feeds/luci/applications/luci-app-zerotier
