e5de979c5fe9b6196baaef950dd1cdf66164f341
Haven Madray <sgpublic2002@gmail.com>
2025-09-20 08:15:56 +0800

    feat(flavor): add Cudy_TR1200

---
+++ flavor/Cudy_TR1200/README.md
@@ -0,0 +1,17 @@
+# Cudy TR1200
+
+## 适用于
+
++ [Cudy TR1200]https://www.cudy.com/zh-cn/products/tr1200-1-0)
+
+## 主题
+
+[jerrykuku/luci-theme-argon: Argon is a clean HTML5 theme for OpenWrt LuCI. Users may setup their own favorite logins, including beautiful pics and customized mp4 videos. (github.com)](https://github.com/jerrykuku/luci-theme-argon)
+
+![screenshot_pc.jpg (1767×938) (raw.githubusercontent.com)](https://raw.githubusercontent.com/jerrykuku/luci-theme-argon/master/Screenshots/screenshot_pc.jpg)
+
+## 管理页
+
+默认地址：`192.168.6.1`
+
+默认密码：`password`
---
+++ flavor/Cudy_TR1200/diy-part1.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# Copyright (c) 2019-2020 P3TERX <https://p3terx.com>
+#
+# This is free software, licensed under the MIT License.
+# See /LICENSE for more information.
+#
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+set -e
+
+. $COMMON_DIY_P1
+
+##    添加你的自定义包    ##
+
+# 添加软件源 sundaqiang/openwrt-packages
+git_clone https://github.com/sundaqiang/openwrt-packages.git package/sundaqiang
+# 添加软件源 nikkinikki-org/OpenWrt-nikki
+git_clone https://github.com/nikkinikki-org/OpenWrt-nikki.git package/nikki
+
+if [ "$USE_LOCAL_PACKAGES" == "y" ]; then
+  # 本地拉取依赖
+  rm -rf package/sgpublic && mkdir -p package/sgpublic
+  cp -r /mnt/documents/OpenWrt/openwrt-packages/* package/sgpublic
+else
+  # 添加软件源 sgpublic/openwrt-packages
+  git_clone https://github.com/sgpublic/openwrt-packages.git package/sgpublic
+fi
+
+
+# 拉取最新 GoLang
+git_clone https://github.com/sbwml/packages_lang_golang ./custom-feeds/packages-sbwml/lang/golang 24.x
+# 更新 v2ray-geodata
+git_clone https://github.com/sbwml/v2ray-geodata ./custom-feeds/packages-sbwml/net/v2ray-geodata
+# 拉取源 luci-app-zerotier
+git_clone https://github.com/immortalwrt/luci ./custom-feeds/luci-immortalwrt $IMMORTALWRT_BRANCH
+
+
+# 拉取主题 luci-theme-argon
+git_clone https://github.com/jerrykuku/luci-theme-argon.git package/jerrykuku/luci-theme-argon
+# 拉取插件 luci-app-argon-config
+git_clone https://github.com/jerrykuku/luci-app-argon-config.git package/jerrykuku/luci-app-argon-config
+# 拉取插件 luci-app-mosdns
+git_clone https://github.com/sbwml/luci-app-mosdns package/sbwml-mosdns
